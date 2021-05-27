de25947141ad686911c6c1edcc18a1838a959799
Yongfu Liao <liao961120@gmail.com>
2021-05-27 13:30:21 +0800

    Create answer.R

---
+++ answer.R
@@ -0,0 +1,44 @@
+library(httr)
+library(rvest)
+library(stringr)
+
+url <- 'https://streetvoice.com/music/charts/24hr/'
+
+html <- GET(url) %>% content()
+
+songs <- html %>% 
+   html_nodes("ul.list-group-song") %>%
+   html_nodes("li")
+
+
+# Rank
+rank <- songs %>% html_nodes("div.work-item-unmber h4") %>% html_text()
+
+# title
+title <- songs %>% 
+   html_nodes("div.work-item-info h4") %>% 
+   html_text() %>%
+   trimws()
+
+# author
+author <- songs %>% 
+   html_nodes("div.work-item-info h5") %>% 
+   html_text()
+
+# Likes
+likes <- songs %>%
+   html_nodes("div.text-right span.js-like-count") %>%
+   html_text() %>%
+   sapply(function(x) {
+      ifelse(str_detect(x, "k"), 
+             as.numeric(str_remove(x, "k"))* 1000, 
+             x)},
+      USE.NAMES = FALSE)
+
+# Merge data
+d <- tibble::tibble(
+   rank = rank,
+   title = title,
+   author = author,
+   likes = likes
+)
