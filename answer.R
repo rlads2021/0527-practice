cd66a58b4dfb5e9f6845ba5dec45d7db04dcbf15
Yongfu Liao <liao961120@gmail.com>
2021-05-27 15:30:01 +0800

    Update answer.R

--- answer.R
+++ answer.R
@@ -3,38 +3,38 @@ library(rvest)
 library(stringr)
 
 # Download web page
-resp <- GET('https://streetvoice.com/music/charts/24hr/') 
+resp <- GET("https://streetvoice.com/music/charts/24hr/")
 resp$status_code
 html <- content(resp)
 
-# Get top 50 songs
-songs <- html %>% 
-   html_nodes("ul.list-group-song") %>%
-   html_nodes("li")
+# Songs
+songs <- html %>% html_nodes("ul.list-group-song > li")
 
 # Rank
 rank <- songs %>% 
-   html_nodes("div.work-item-unmber h4") %>% 
-   html_text()
-
-# title
-title <- songs %>% 
-   html_nodes("div.work-item-info h4") %>% 
+   html_nodes(".work-item-unmber h4") %>% 
    html_text() %>%
-   trimws()
+   as.integer()
 
-# author
+# Title
+title <- songs %>% 
+   html_nodes(".work-item-info h4 a") %>% 
+   html_text() %>% trimws()
+
+# Author
 author <- songs %>% 
-   html_nodes("div.work-item-info h5") %>% 
-   html_text()
+   html_nodes(".work-item-info h5 a") %>% 
+   html_text() %>% trimws()
 
 # Likes
-likes <- songs %>%
-   html_nodes("div.text-right span.js-like-count") %>%
+likes <- songs %>% 
+   html_nodes(".text-right button span.js-like-count") %>%
    html_text() %>%
    sapply(function(x) {
-      ifelse(str_detect(x, "k"), 
-             as.numeric(str_remove(x, "k")) * 1000, x)
+      ifelse( str_detect(x, "k"), 
+              (str_remove(x, "k") %>% as.numeric()) * 1000,
+              as.numeric(x)
+              )
    }, USE.NAMES = FALSE)
 
 # Merge data
